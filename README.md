0cf4ae1e96e70bcb46d681a922accf107845d25a
Adeel Ahmad <adeel@hashicorp.com>
2021-01-07 13:27:22 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,19 @@
+## Requirements
+
+No requirements.
+
+## Providers
+
+| Name | Version |
+|------|---------|
+| `vault` | n/a |
+
+## Inputs
+
+| Name | Description | Type | Default | Required |
+|------|-------------|------|---------|:--------:|
+| `entity_ids` | List of Vault Identity Entity Member IDs | `list(any)` | `[]` | no |
+
+## Outputs
+
+No output.
---
+++ main.tf
@@ -0,0 +1,26 @@
+resource "vault_aws_secret_backend" "default" {}
+
+data "vault_policy_document" "default" {
+  rule {
+    path         = "aws/creds/{{identity.entity.metadata.env}}-{{identity.entity.metadata.service}}"
+    capabilities = ["read"]
+    description  = "Allow generating credentials"
+  }
+}
+
+resource "vault_policy" "default" {
+  name   = "aws-creds"
+  policy = data.vault_policy_document.default.hcl
+}
+
+resource "vault_identity_group" "default" {
+  name              = "aws-creds"
+  type              = "internal"
+  policies          = ["default", vault_policy.default.name]
+  member_entity_ids = var.entity_ids != [] ? var.entity_ids : [vault_identity_entity.default.id]
+
+}
+
+resource "vault_identity_entity" "default" {
+  name = "default"
+}
