a2fa57de96e12c708457b875d43c77028b10110d
Adeel Ahmad <8452076+devops-adeel@users.noreply.github.com>
2021-01-13 10:16:35 +0000

    Feature/module update (#1)
    * tf fmt
    
    * updated module to accept groups as members
    
    Co-authored-by: Adeel Ahmad <adeel@hashicorp.com>

---
+++ outputs.tf
@@ -0,0 +1,6 @@
+output "vault_identity_group" {
+  description = "JSON data of the Vault Identity Group, including list of member entities"
+  value       = data.vault_identity_group.default.data_json
+}
+
+
